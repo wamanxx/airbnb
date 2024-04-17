216ea17e9ff432abb0d7d6c1c4b1e2cc083f5e0f
wamanxx <wamanxpro@gmail.com>
2024-04-17 13:35:30 +0100

    Commit 17/04

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,22 +3,21 @@ import React, { useMemo, useState } from 'react'
 import { Link, Stack } from 'expo-router'
 import ExploreHeader from '@/components/ExploreHeader'
 import Listings from '@/components/Listings'
-import ListingsData from '@/assets/data/air-bnb-listings.json'
+import ListingsData from '@/assets/data/airbnb-listings.json'
 const Page = () => {
   const [category, setCategory] = useState('Tiny Home')
   const items = useMemo(()=>ListingsData as any, [])
   const onDataChanged = (category: string) => {
     setCategory(category)
   }
-  return (
-    // <View style={{ flex: 1, marginTop: 150, backgroundColor: 'red', borderColor: 'red', borderWidth: 10 }}>
-    <View style={{ flex: 1 }}>
+    return (
+    <View style={{ flex: 1, marginTop: 150 }}>
       <Stack.Screen
         options={{
           header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
 
         }} />
-      <Listings listings={items} category={category} />²
+      <Listings listings={items} category={category} />
     </View>
   )
 }
--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,23 +1,76 @@
-import { View, Text, FlatList } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, FlatList, ListRenderItem, TouchableOpacity, StyleSheet, Image } from 'react-native'
+import React, { useEffect, useState, useRef } from 'react'
 import { defaultStyles } from '@/constants/Styles';
+import { Link } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
-interface Props{
+
+
+interface Props {
   listings: any[];
   category: string;
 
 }
 
-const Listings = ({listings, category}: Props) => {
-  useEffect(()=>{
-    console.log(listings.length)
-  },[category])
+const Listings = ({ listings: items, category }: Props) => {
+  const [loading, setLoading] = useState(false)
+  const listRef = useRef<FlatList>(null)
+  useEffect(() => {
+    console.log(items.length)
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+
+    }, 200)
+  }, [category])
+
+  const renderRow: ListRenderItem<any> = ({ item }) => (
+    <Link href={`/listing/${item.id}`} asChild>
+      <TouchableOpacity>
+        <View style={styles.listing}>
+          <Image source={{ uri: item.medium_url }} style={styles.image} />
+          <TouchableOpacity style={{
+            position: 'absolute',
+            right:30,
+            top:30
+          }}>
+            <Ionicons name='heart-outline' size={24} color={'black'}/>
+          </TouchableOpacity>
+          <View style={{flexDirection: 'row', justifyContent:'space-between'}}>
+            <Text style={{fontSize: 16, fontFamily: 'mon-b'}}>{item.name}</Text>
+            <View style={{flexDirection:'row', gap:5}}>
+              <Text style={{fontFamily: 'mon-b'}}>{item.review_scores_rating/20}</Text>
+              <Ionicons name="star" size={16}/>
+            </View>
+
+          </View>
+            <Text style={{fontFamily: 'mon-l'}}>{item.room_type}</Text>
+            <View style={{flexDirection:'row', gap:5}}>
+              <Text style={{fontFamily:'mon-b'}}>{item.price} EUR</Text>
+              <Text style={{fontFamily:'mon'}}>/ Nuit</Text>
+
+            </View>
+        </View>
+      </TouchableOpacity>
+    </Link>
+  )
   return (
     <View style={defaultStyles.container} >
-      <FlatList/>
+      <FlatList data={loading ? [] : items} ref={listRef} renderItem={renderRow} />
 
     </View>
   )
 }
-
+const styles = StyleSheet.create({
+  listing: {
+    padding: 16,
+    gap:10,
+    marginVertical: 16,
+  },
+  image: {
+    width: '100%',
+    height: 300,
+    borderRadius: 10
+  }
+})
 export default Listings
